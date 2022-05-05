56bfb0ee8663359dc233ed968aa82fcc7d9187a3
IgnacioMGarrido <nama1510@gmail.com>
2022-05-05 23:44:44 +0200

    Fixed Compile Shaders batch file to works with VK_SDK_PATH

--- src/rendering/vulkan/shaders/compile.bat
+++ src/rendering/vulkan/shaders/compile.bat
@@ -1,5 +1,5 @@
 @echo off
 @echo Building Vulkan Shaders
-C:\VulkanSDK\1.3.211.0\Bin\glslc.exe ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.vert -o ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.vert.spv
-C:\VulkanSDK\1.3.211.0\Bin\glslc.exe ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.frag -o ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.frag.spv
+%VK_SDK_PATH%\Bin\glslc.exe ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.vert -o ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.vert.spv
+%VK_SDK_PATH%\Bin\glslc.exe ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.frag -o ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.frag.spv
 @echo Shaders compilation finished
