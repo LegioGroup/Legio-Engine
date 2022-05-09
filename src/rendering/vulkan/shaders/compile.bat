e827ee4ce084dbae63b11d04a886ff11d35f6bce
IgnacioMGarrido <nama1510@gmail.com>
2022-05-09 13:39:33 +0200

    Added imgui submodule

--- src/rendering/vulkan/shaders/compile.bat
+++ src/rendering/vulkan/shaders/compile.bat
@@ -1,5 +1,11 @@
 @echo off
-@echo Building Vulkan Shaders
-%VK_SDK_PATH%\Bin\glslc.exe ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.vert -o ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.vert.spv
-%VK_SDK_PATH%\Bin\glslc.exe ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.frag -o ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.frag.spv
-@echo Shaders compilation finished
+
+IF EXIST %VK_SDK_PATH%\Bin\glslc.exe (
+    @echo Building Vulkan Shaders
+    %VK_SDK_PATH%\Bin\glslc.exe ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.vert -o ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.vert.spv
+    %VK_SDK_PATH%\Bin\glslc.exe ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.frag -o ../../../external/engine/src/rendering/vulkan/shaders/basic_shader.frag.spv
+    @echo Shaders compilation finished
+) ELSE (
+    echo "Coudn't find Vulkan SDK. Make sure Vulkan is installed and VK_SDK_PATH is setup correctly in env variables"
+)
+
