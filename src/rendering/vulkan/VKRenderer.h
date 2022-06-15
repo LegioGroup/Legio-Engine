c2ab6b6133f81b09ac21a00b5d269267bd4b842a
IgnacioMGarrido <nama1510@gmail.com>
2022-06-15 17:05:13 +0200

    Fixed missing destructor in renderer

--- src/rendering/vulkan/VKRenderer.h
+++ src/rendering/vulkan/VKRenderer.h
@@ -15,8 +15,6 @@ namespace LG
         VKRenderer(const VKRenderer&) = delete;
         VKRenderer& operator=(const VKRenderer&) = delete;
 
-        virtual ~VKRenderer();
-
         virtual void Init(RendererSettings settings) override;
         virtual void Shutdown() override;
         virtual void RenderFrame() override;
